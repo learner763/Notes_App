e5fdbf4ec2a094202362cf5a609e747047534285
umer <umeraftab763@gmail.com>
2025-12-17 11:29:43 +0500

    edit settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,13 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "build" / "static"
-]
+]'''
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- 
